c01b7f3c2782d151b2737c86ff73fde725fae3a0
Reginald Appiah <regappsllc@gmail.com>
2020-10-24 04:43:31 -0400

    Merge Attempt 2

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
     return StreamProvider.value(
       value: AuthService().user,
       child: MaterialApp(
-        themeMode: ThemeMode.light,
+        themeMode: ThemeMode.dark,
         darkTheme: darkMode(),
         theme: GSConstants.defaultTheme,
         home: Wrapper(),
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   ThemeData darkMode() {
-    Color favColor = Colors.blue;
+    Color favColor = Colors.green;
     return ThemeData(
       primaryColor: favColor,
       accentColor: favColor,
