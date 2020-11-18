50353c603f82eee94ad557f242e2edeb82ea1c85
Reginald Appiah <regappsllc@gmail.com>
2020-11-18 14:01:04 -0500

    Removed Wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:gradeslide/gswrapper.dart';
 import 'package:gradeslide/logic/gsconstants.dart';
 import 'package:gradeslide/login/auth_service.dart';
+import 'package:gradeslide/pages/courses/courses_page.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'logic/database_service.dart';
+import 'login/sign_in.dart';
 
 void main() {
+  Firestore.instance.settings(persistenceEnabled: true);
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    Firestore.instance.settings(persistenceEnabled: true);
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MultiProvider(
@@ -40,7 +29,19 @@ class _MyAppState extends State<MyApp> {
         themeMode: ThemeMode.dark,
         darkTheme: GSConstants.darkMode(Colors.blue),
         theme: GSConstants.defaultTheme(Colors.blue),
-        home: Wrapper(),
+        home: FutureBuilder(
+            future: Future.delayed(Duration(milliseconds: 1000)),
+            builder: (context, snapshot) {
+              FirebaseUser user = Provider.of<FirebaseUser>(context);
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (user != null) {
+                  return CoursesPage();
+                }
+                return SignInScreen();
+              } else {
+                return Scaffold(body: Center(child: CircularProgressIndicator()));
+              }
+            }),
         debugShowCheckedModeBanner: false,
       ),
     );
